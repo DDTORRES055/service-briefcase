ab2c658a92e81f4d9968ca90f4fd59eefbb9adb5
Daniel Diaz <ddtorres055@gmail.com>
2022-06-14 13:51:49 -0500

    Remove uncool stuff from webapp

--- webapp/src/routes/sidebar.js
+++ webapp/src/routes/sidebar.js
@@ -6,10 +6,15 @@
  * `routes/index.js`
  */
 const routes = [
+  // {
+  //   path: '/app/dashboard', // the url
+  //   icon: 'HomeIcon', // the component being exported from icons/index.js
+  //   name: 'Dashboard', // name that appear in Sidebar
+  // },
   {
-    path: '/app/dashboard', // the url
-    icon: 'HomeIcon', // the component being exported from icons/index.js
-    name: 'Dashboard', // name that appear in Sidebar
+    path: '/app/services',
+    icon: 'FormsIcon',
+    name: 'Servicios',
   },
   {
     path: '/app/departments',
@@ -56,68 +61,63 @@ const routes = [
     icon: 'FormsIcon',
     name: 'DBMS',
   },
-  {
-    path: '/app/services',
-    icon: 'FormsIcon',
-    name: 'Servicios',
-  },
-  {
-    path: '/app/forms',
-    icon: 'FormsIcon',
-    name: 'Forms',
-  },
-  {
-    path: '/app/cards',
-    icon: 'CardsIcon',
-    name: 'Cards',
-  },
-  {
-    path: '/app/charts',
-    icon: 'ChartsIcon',
-    name: 'Charts',
-  },
-  {
-    path: '/app/buttons',
-    icon: 'ButtonsIcon',
-    name: 'Buttons',
-  },
-  {
-    path: '/app/modals',
-    icon: 'ModalsIcon',
-    name: 'Modals',
-  },
-  {
-    path: '/app/tables',
-    icon: 'TablesIcon',
-    name: 'Tables',
-  },
-  {
-    icon: 'PagesIcon',
-    name: 'Pages',
-    routes: [
-      // submenu
-      {
-        path: '/login',
-        name: 'Login',
-      },
-      {
-        path: '/create-account',
-        name: 'Create account',
-      },
-      {
-        path: '/forgot-password',
-        name: 'Forgot password',
-      },
-      {
-        path: '/app/404',
-        name: '404',
-      },
-      {
-        path: '/app/blank',
-        name: 'Blank',
-      },
-    ],
-  },
+  // {
+  //   path: '/app/forms',
+  //   icon: 'FormsIcon',
+  //   name: 'Forms',
+  // },
+  // {
+  //   path: '/app/cards',
+  //   icon: 'CardsIcon',
+  //   name: 'Cards',
+  // },
+  // {
+  //   path: '/app/charts',
+  //   icon: 'ChartsIcon',
+  //   name: 'Charts',
+  // },
+  // {
+  //   path: '/app/buttons',
+  //   icon: 'ButtonsIcon',
+  //   name: 'Buttons',
+  // },
+  // {
+  //   path: '/app/modals',
+  //   icon: 'ModalsIcon',
+  //   name: 'Modals',
+  // },
+  // {
+  //   path: '/app/tables',
+  //   icon: 'TablesIcon',
+  //   name: 'Tables',
+  // },
+  // {
+  //   icon: 'PagesIcon',
+  //   name: 'Pages',
+  //   routes: [
+  //     // submenu
+  //     {
+  //       path: '/login',
+  //       name: 'Login',
+  //     },
+  //     {
+  //       path: '/create-account',
+  //       name: 'Create account',
+  //     },
+  //     {
+  //       path: '/forgot-password',
+  //       name: 'Forgot password',
+  //     },
+  //     {
+  //       path: '/app/404',
+  //       name: '404',
+  //     },
+  //     {
+  //       path: '/app/blank',
+  //       name: 'Blank',
+  //     },
+  //   ],
+  // },
 ]
 
 export default routes
